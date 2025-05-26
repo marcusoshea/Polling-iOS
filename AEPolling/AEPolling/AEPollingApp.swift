b651dfdacaf0f370044a4924acb58c1cac1ad253
Marcus O'Shea <marcusoshea100@gmail.com>
2025-05-26 09:47:30 -0400

    Initial commit

---
+++ AEPolling/AEPolling/AEPollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AEPollingApp.swift
+//  AEPolling
+//
+//  Created by Marcus O'Shea on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct AEPollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AEPolling/AEPollingTests/AEPollingTests.swift
@@ -0,0 +1,17 @@
+//
+//  AEPollingTests.swift
+//  AEPollingTests
+//
+//  Created by Marcus O'Shea on 5/26/25.
+//
+
+import Testing
+@testable import AEPolling
+
+struct AEPollingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
